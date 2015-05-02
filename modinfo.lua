43f6ab0ae714a36318ad67adfd66006a45241d0f
Gajo Petrovic <gajo01@gmail.com>
2015-05-02 14:52:48 +0900

    initial

---
+++ modinfo.lua
@@ -0,0 +1,14 @@
+return {
+  name='Scenario Editor GBS Test',
+  description='Scenario Editor for GBS Test',
+  shortname='SE_GBS_TEST',
+  version='git',
+  mutator='Official',
+  game='Scenario Editor GBS Test',
+  shortGame='SE_GBS_TEST',
+  modtype=1,
+  depend = {
+    'GBS Test 004',
+	'Scenario Editor Core git',
+  },
+}
